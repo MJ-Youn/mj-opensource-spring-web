1aff777d3e038c98cdce829be552a0b3d86370a9
MJ Youn <yun0244@naver.com>
2022-10-04 15:42:59 +0900

    버전 변경

---
+++ src/main/java/io/github/mjyoun/spring/web/service/CSVService.java
@@ -0,0 +1,86 @@
+package io.github.mjyoun.spring.web.service;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.text.NumberFormat;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.stereotype.Service;
+
+import com.opencsv.CSVWriter;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * CSV 파일 관련된 서비스
+ * 
+ * @author MJ Youn
+ * @since 2022. 06. 21.
+ */
+@Slf4j
+@Service(CSVService.QUALIFIER_NAME)
+public class CSVService {
+
+    public static final String QUALIFIER_NAME = "io.github.mjyoun.spring.web.service.CSVService";
+
+    /**
+     * CSV 파일 정보를 생성하는 함수
+     * 
+     * @param fileName
+     *            확장자를 제외한, 다운로드할 파일 이름 (확장자는 자동으로 붙혀줌)
+     * @param headers
+     *            헤더 목록. null일 경우 헤더가 없는 형태의 csv 파일
+     * @param datas
+     *            데이터 목록
+     * @param separator
+     *            구분자
+     * @param quote
+     *            따옴표, 없을 경우 따옴표 하지 않음. 있을 경우 무조건 따옴표로 묶음
+     * @return CSV 파일 데이터가 들어있는 byte array
+     * 
+     * @throws IOException
+     *             CSV FileWriter 생성 실패
+     * 
+     * @author MJ Youn
+     * @since 2022. 06. 21.
+     */
+    public byte[] createCSV(@NotNull String fileName, String[] headers, List<String[]> datas, char separator, char quote) throws IOException {
+        final String methodName = "CSVService#createCSV";
+
+        // 따옴표가 설정되어 있는지 여부
+        boolean wasSetQuote = quote != '\0';
+
+        StringWriter writer = new StringWriter();
+        // bom encoding 추가
+        writer.append("\ufeff");
+        try (CSVWriter csvWriter = new CSVWriter(writer, //
+                separator == '\0' ? CSVWriter.DEFAULT_SEPARATOR : separator, // 구분자
+                quote == '\0' ? CSVWriter.DEFAULT_QUOTE_CHARACTER : quote, // quote
+                CSVWriter.DEFAULT_ESCAPE_CHARACTER, // 예외 처리 문자
+                CSVWriter.DEFAULT_LINE_END // 라인 끝 문자
+        )) {
+            // header 입력
+            if (headers == null) {
+                log.debug("[{}] 헤더가 없는 CSV 파일", methodName);
+            } else {
+                csvWriter.writeNext(headers);
+                log.debug("[{}] 헤더 설정 완료 [header count: {}]", methodName, headers.length);
+            }
+
+            // body 입력
+            if (wasSetQuote) {
+                log.debug("[{}] 따옴표가 설정되어 있어, 모든 데이터를 따옴표로 감싸서 출력합니다. [quote: {}]", methodName, quote);
+            }
+
+            csvWriter.writeAll(datas, wasSetQuote);
+            log.debug("[{}] 데이터 설정 완료 [data count: {}]", methodName, NumberFormat.getInstance().format(datas.size()));
+        }
+
+        String fileContent = writer.toString();
+
+        return fileContent.getBytes();
+    }
+
+}
---
+++ src/main/java/io/github/mjyoun/spring/web/service/DownloadService.java
@@ -0,0 +1,197 @@
+package io.github.mjyoun.spring.web.service;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Download 기능 관련된 유틸리티 클래스
+ * 
+ * @author MJ Youn
+ * @since 2022. 06. 21.
+ */
+@Slf4j
+@Service(DownloadService.QUALIFIER_NAME)
+public class DownloadService {
+
+    public static final String QUALIFIER_NAME = "io.github.mjyoun.spring.web.service.DownloadService";
+
+    private CSVService csvService;
+
+    /**
+     * (non-javadoc)
+     * 
+     * @param csvService
+     *            {@link CSVService}
+     * 
+     * @author MJ Youn
+     * @since 2022. 06. 21.
+     */
+    protected DownloadService(@Qualifier(CSVService.QUALIFIER_NAME) CSVService csvService) {
+        this.csvService = csvService;
+    }
+
+    /**
+     * @see DownloadService#downloadCsv(String, String[], List, char, char, HttpServletResponse)
+     * 
+     * @param fileName
+     *            확장자를 제외한 파일 이름 (확장자는 '.csv' 형태로 자동으로 붙혀줌)
+     * @param headers
+     *            헤더 목록. null일 경우 헤더가 없는 형태의 csv 파일
+     * @param datas
+     *            데이터 목록
+     * @param response
+     *            {@link HttpServletResponse}
+     * @throws IOException
+     *             FileWriter 닫는중에 오류 발생
+     * 
+     * @author MJ Youn
+     * @since 2022. 06. 21.
+     */
+    public void downloadCSV(@NotNull String fileName, String[] headers, List<String[]> datas, HttpServletResponse response) throws IOException {
+        this.downloadCsv(fileName, headers, datas, ',', '\0', response);
+    }
+
+    /**
+     * Array 데이터를 기준으로 CSV 파일로 다운로드해주는 함수
+     * 
+     * @param fileName
+     *            확장자를 제외한, 다운로드할 파일 이름 (확장자는 자동으로 붙혀줌)
+     * @param headers
+     *            헤더 목록. null일 경우 헤더가 없는 형태의 csv 파일
+     * @param datas
+     *            데이터 목록
+     * @param separator
+     *            구분자
+     * @param quote
+     *            따옴표, 없을 경우 따옴표 하지 않음. 있을 경우 무조건 따옴표로 묶음
+     * @param response
+     *            {@link HttpServletResponse}
+     * 
+     * @throws IOException
+     *             FileWriter 닫는중에 오류 발생
+     * 
+     * @author MJ Youn
+     * @since 2022. 06. 21.
+     */
+    public void downloadCsv(@NotNull String fileName, String[] headers, List<String[]> datas, char separator, char quote,
+            HttpServletResponse response) throws IOException {
+        final String methodName = "DownloadService#downloadCSV";
+
+        fileName = new StringBuffer(fileName).append(".csv").toString();
+        log.debug("[{}] 다운로드 파일 이름: {}", methodName, fileName);
+
+        // csv content 생성
+        byte[] contentBytes = this.csvService.createCSV(fileName, headers, datas, separator, quote);
+        InputStream inputStream = new ByteArrayInputStream(contentBytes);
+
+        // download를 위한 disposition 생성
+        String contentDisposition = new StringBuffer("attachment; filename=\"") //
+                .append(URLEncoder.encode(fileName, "UTF-8").replace("+", "%20")) //
+                .append("\"") //
+                .toString();
+
+        try {
+            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+            response.setContentLength(contentBytes.length);
+            response.setHeader("Content-Disposition", contentDisposition);
+
+            FileCopyUtils.copy(inputStream, response.getOutputStream());
+            log.debug("[{}] CSV 파일 다운로드 요청 성공 [file name: {}]", methodName, fileName);
+        } catch (IOException ioe) {
+            log.error("[{}] CSV 파일 다운로드 실패 [msg: {}]", methodName, ioe.getMessage());
+            ioe.printStackTrace();
+        }
+    }
+
+    /**
+     * 파일 다운로드
+     * 
+     * @param filePath
+     *            파일 경로. 저장되어 있는 파일 이름으로 다운로드 요청
+     * @param response
+     *            {@link HttpServletResponse}
+     * 
+     * @throws FileNotFoundException
+     *             다운로드할 파일에 문제가 있을 경우
+     * @throws UnsupportedEncodingException
+     *             파일 이름 인코딩 설정이 잘못 되었을 경우. 발생하지 않을 듯..
+     * 
+     * @author MJ Youn
+     * @since 2022. 08. 12.
+     */
+    public void downloadFile(@NotNull Path filePath, @NotNull HttpServletResponse response)
+            throws FileNotFoundException, UnsupportedEncodingException {
+        this.downloadFile(filePath.toFile().getName(), filePath, response);
+    }
+
+    /**
+     * 파일 다운로드
+     * 
+     * @param downloadFileName
+     *            다운로드할 파일의 이름
+     * @param filePath
+     *            파일 경로
+     * @param response
+     *            {@link HttpServletResponse}
+     * 
+     * @throws FileNotFoundException
+     *             다운로드할 파일에 문제가 있을 경우
+     * @throws UnsupportedEncodingException
+     *             파일 이름 인코딩 설정이 잘못 되었을 경우. 발생하지 않을 듯..
+     * 
+     * @author MJ Youn
+     * @since 2022. 08. 12.
+     */
+    public void downloadFile(@NotBlank String downloadFileName, @NotNull Path filePath, @NotNull HttpServletResponse response)
+            throws FileNotFoundException, UnsupportedEncodingException {
+        final String methodName = "DownloadService#downloadFile";
+
+        if (!Files.exists(filePath)) {
+            String msg = "존재하지 않는 파일입니다.";
+            log.error("[{}] {} [path: {}]", methodName, msg, filePath);
+            throw new FileNotFoundException(msg);
+        } else if (Files.isDirectory(filePath)) {
+            String msg = "디렉토리는 다운로드 할 수 없습니다.";
+            log.error("[{}] {} [path: {}]", methodName, msg, filePath);
+            throw new FileNotFoundException(msg);
+        } else {
+            String contentDisposition = new StringBuffer("attachment; filename=\"") //
+                    .append(URLEncoder.encode(downloadFileName, "UTF-8").replace("+", "%20")) //
+                    .append("\"") //
+                    .toString();
+
+            log.debug("[{}] 다운로드 할 파일 이름: {}", methodName, downloadFileName);
+
+            response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+            response.setContentLength((int) filePath.toFile().length());
+            response.setHeader("Content-Disposition", contentDisposition);
+
+            try {
+                Files.copy(filePath, response.getOutputStream());
+                log.debug("[{}] 파일 다운로드 요청 성공 [file name: {}]", methodName, downloadFileName);
+            } catch (IOException ioe) {
+                log.error("[{}] 파일 다운로드 실패 [msg: {}]", methodName, ioe.getMessage());
+                ioe.printStackTrace();
+            }
+        }
+    }
+
+}
